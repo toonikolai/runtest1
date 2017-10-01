18e2f93503456e70aa2edd4774bfc672851bca0e
Bradley Doering <bradley.doering@gmail.com>
2017-10-01 00:12:39 -0700

    more complete profile page

--- app/src/main/java/com/example/nikolaistakheiko/runtest1/ProfileActivity.java
+++ app/src/main/java/com/example/nikolaistakheiko/runtest1/ProfileActivity.java
@@ -2,6 +2,10 @@ package com.example.nikolaistakheiko.runtest1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +15,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -25,16 +30,41 @@ public class ProfileActivity extends AppCompatActivity {
         mDrawerLayout2 = (DrawerLayout) findViewById(R.id.drawer_layout2);
         buttonSetUp();
         spinnerSetUp();
+        spinnerSetUp2();
+        terrainSetUp();
+
+//        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.stlsm);
+//        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+//        roundedBitmapDrawable.setCircular(true);
+//        imageView.setImageDrawable(roundedBitmapDrawable);
     }
 
     private void spinnerSetUp() {
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.Gender);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.gender_array, android.R.layout.simple_spinner_item);
 // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    private void spinnerSetUp2() {
+        Spinner spinner = (Spinner) findViewById(R.id.RunPartner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    private void terrainSetUp() {
+        Spinner spinner = (Spinner) findViewById(R.id.Terrain);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.terrain_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
 
